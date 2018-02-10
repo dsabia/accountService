7a72ba971eb27c49e5cfd6ecb610c7411a0ebcc3
Daniele <dsabia@hotmail.it>
2018-02-10 12:26:15 +0100

    added log setting plus some code clenaup

--- src/main/java/com/ing/technicaltest/exception/UserNotFoundException.java
+++ src/main/java/com/ing/technicaltest/exception/UserNotFoundException.java
@@ -2,13 +2,9 @@ package com.ing.technicaltest.exception;
 
 public class UserNotFoundException extends RuntimeException {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -1036165348305145617L;
 
 	public UserNotFoundException(String message) {
 		super(message);
 	}
-	
 }
